314e75caa62f6fb764275a4211c786cea16c1eb1
Sharee-f <158843631+Sharee-f@users.noreply.github.com>
2024-07-24 17:16:07 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+// 
+const searchfield=document.querySelector('.search input');
+const searchbutton=document.querySelector('.search button');
+
+
+searchbutton.addEventListener("click",()=>{
+        fetchdata(searchfield.value)
+})
+const fetchdata=async (target)=>{
+        let url=`http://api.weatherapi.com/v1/current.json?key=0604fc3681714f21b9682906242207&q=${target}&aqi=no`
+        const result=await fetch(url);
+        var data = await result.json();
+        console.log(data)
+        document.querySelector('.tempreature').innerHTML=data.current.temp_c;
+        document.querySelector('.location').innerHTML=data.location.name;
+        document.querySelector('.time').innerHTML=data.location.localtime;
+        document.querySelector('.condition').innerHTML=data.current.condition.text;
+
+}
